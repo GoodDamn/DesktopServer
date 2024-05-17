e5f05c7094340f3c004059fed894c9593f4385eb
GoodDamn <grigorydum@gmail.com>
2024-05-17 09:38:12 +0300

    Returning static file

--- src/main/kotlin/services/HTTPResponse.kt
+++ src/main/kotlin/services/HTTPResponse.kt
@@ -4,6 +4,9 @@ import good.damn.filesharing.http.HTTPHeaders
 import good.damn.filesharing.http.HTTPPath
 import good.damn.filesharing.http.paths.HTTPPathActionEmail
 import good.damn.filesharing.http.paths.HTTPPathDocument
+import good.damn.filesharing.utils.FileUtils
+import java.io.File
+import java.io.FileInputStream
 import java.io.OutputStream
 
 class HTTPResponse {
@@ -21,14 +24,47 @@ class HTTPResponse {
             to: OutputStream,
             httpPath: HTTPPath
         ) {
-            PATHS[httpPath.path]?.execute(
+            val res = PATHS[httpPath.path]?.execute(
                 to,
                 httpPath
-            ) ?: {
+            )
+
+            if (res != null) {
+                return
+            }
+
+            val serverDir = FileUtils
+                .getDocumentsFolder()
+
+            val staticFile = File("$serverDir/${httpPath.path}")
+            if (!(staticFile.exists() && staticFile.isFile)) {
                 HTTPHeaders.error(
                     to
                 )
+                return
             }
+
+            val p = httpPath.path
+
+            val fileName = p
+                .substring(
+                    p.lastIndexOf("/") + 1
+                )
+
+            HTTPHeaders.file(
+                to,
+                staticFile.length().toInt(),
+                fileName
+            )
+
+            val inp = FileInputStream(
+                staticFile
+            )
+
+            FileUtils.copyBytes(
+                inp,
+                to
+            )
         }
     }
 }
